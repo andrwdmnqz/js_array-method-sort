366e847429555d2cc747c772de8851afa1d5154a
Andrew <andresshokhirev@gmail.com>
2025-04-09 12:26:44 +0300

    Update

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,7 +6,15 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function (
     compareFunction = (a, b) => {
-      return String(a) > String(b);
+      if (String(a) > String(b)) {
+        return 1;
+      }
+
+      if (String(a) < String(b)) {
+        return -1;
+      }
+
+      return 0;
     },
   ) {
     for (let i = 0; i < this.length; i++) {
